72fc6402abff17b3d918425dfcadc1bbf5343311
doodieb15 <doodieb15@hotmail.com>
2017-09-29 20:19:44 +0200

    release

--- sh-files/getCO2.sh
+++ sh-files/getCO2.sh
@@ -9,7 +9,7 @@ if [ $light_id -le 25 ]; then
 #echo $light_id
 #echo $light_value
 #echo $server_ip : $server_port
-myurl="http://$server_ip:$server_port/goform/ReadWrite?redirect=lights%2Fbrightness.asp&variable=Zimmer%5B$light_id%5D.Temp_ist_flt&value="$light_value"000&read=Read"
+myurl="http://$server_ip:$server_port/goform/ReadWrite?redirect=lights%2Fbrightness.asp&variable=Zimmer%5B$light_id%5D.Klima.CO2&value="$light_value"000&read=Read"
 
 if [ $? -gt 0 ]; then
 	echo curl error
@@ -19,17 +19,13 @@ else
 resp=$(curl  -s "$myurl" | sed -e "s/%2e/\./g"   | grep -oP "\w*val=\w[ ]" ) #  | head -1l | cut -c5-) # | sed -i -e 's/%2e/\./g'
 resp1=$(curl -s "$myurl" | sed -e "s/%2e/\./g")
 
-
 #echo $resp1
 resp2=${resp1% &*}
 #echo $resp2
 
-
 resp=${resp2##*val=}
 
-resp2=${resp//%2d/-}
-
-echo $resp2
+echo $resp
 
 fi
 fi
--- sh-files/setlight.sh
+++ sh-files/setlight.sh
@@ -12,11 +12,31 @@ do
       if [ $light_value -ge 0 ]; then
         if [ $light_value -le 100 ]; then
           myurl="http://$server_ip:$server_port/goform/ReadWrite?redirect=lights%2Fbrightness.asp&variable=V_soll%5B$light_id%5D&value="$light_value"000&write=Write"
-          curl -s "$myurl"
+          resp=$(curl -s $myurl)
           if [ $? -gt 0 ]; then
             echo curl error
           fi
+	  
         fi
+	if [ $light_value -eq 110 ]; then
+	   sleep 0.1
+	   myurl="http://$server_ip:$server_port/goform/ReadWrite?redirect=lights%2Fbrightness.asp&variable=V_soll%5B$light_id%5D&value="$light_value"000&read=Read"
+	   resp=$(curl  -s "$myurl" | grep -oh "\w*val=\w*"  | head -1l | cut -c5-)
+	   resp1=110
+	   if [ $? -gt 0 ]; then
+    	   	echo curl error
+			else
+		resp1=`expr $resp / 1000` 
+     	   fi
+	   if [ $resp1 -eq 0 ]; then
+		myurl="http://$server_ip:$server_port/goform/ReadWrite?redirect=lights%2Fbrightness.asp&variable=V_soll%5B$light_id%5D&value=100000&write=Write"
+          	resp=$(curl -s $myurl)
+          	if [ $? -gt 0 ]; then
+            		echo curl error
+          	fi
+		
+	   fi
+	fi	
       fi
     fi
   fi
